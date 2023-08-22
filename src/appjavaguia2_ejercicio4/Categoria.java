02ac2cc1c3619e3501a29f52b82e1ec30c852b6f
Tomas Abatedaga Biole <abatedagatomas@gmail.com>
2023-08-22 00:10:38 -0300

    Initial commit

---
+++ src/appjavaguia2_ejercicio4/Categoria.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appjavaguia2_ejercicio4;
+
+/**
+ *
+ * @author abate
+ */
+public enum Categoria {
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA,
+    
+}
